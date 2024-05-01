3b8963f986bd86de7bf19293f6eeb9abdc2c55da
ranvir <ranvir.techneai@gmal.com>
2024-05-01 18:18:20 +0530

    profile ui revamp

--- lib/components/sizebox.dart
+++ lib/components/sizebox.dart
@@ -2,23 +2,25 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 class HeightSizedBox extends StatelessWidget {
-  const HeightSizedBox({super.key});
+  final double? height;
+  const HeightSizedBox({Key? key, this.height}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 20,
+    return SizedBox(
+      height: height ?? 20,
     );
   }
 }
 
 class WidthSizedBox extends StatelessWidget {
-  const WidthSizedBox({super.key});
+  final double? width;
+  const WidthSizedBox({Key? key, this.width}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 10,
+    return SizedBox(
+      width: width ?? 20,
     );
   }
 }
--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ticketing_system/components/sizebox.dart';
+import 'package:ticketing_system/constants/colors.dart';
 import 'package:ticketing_system/models/login_model.dart';
+import 'package:ticketing_system/models/user_model.dart';
+import 'package:ticketing_system/view/components/profile_field_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   final LoginResponse loginResponse;
@@ -19,19 +24,33 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-                'Name: ${user.firstName} ${user.middleName ?? ''} ${user.lastName}'),
-            Text('Email: ${user.emailId}'),
-            Text('Contact No: ${user.contactNo}'),
-            Text('Whatsapp No: ${user.whatsAppContactNo}'),
-            Text('Role: ${user.roleName}'),
-            Text('Department: ${user.departmentName}'),
-            Text('Designation: ${user.designation}'),
-            // Add more fields as needed
-          ],
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ProfileFieldWidget(
+                label: 'Name',
+                value:
+                    '${user.firstName} ${user.middleName ?? ''} ${user.lastName}',
+                icon: Icons.person,
+              ),
+
+              ProfileFieldWidget(
+                label: 'Email',
+                value: user.emailId,
+                icon: Icons.email,
+              ),
+              // Text(
+              //     'Name: ${user.firstName} ${user.middleName ?? ''} ${user.lastName}'),
+              // Text('Email: ${user.emailId}'),
+              // Text('Contact No: ${user.contactNo}'),
+              // Text('Whatsapp No: ${user.whatsAppContactNo}'),
+              // Text('Role: ${user.roleName}'),
+              // Text('Department: ${user.departmentName}'),
+              // Text('Designation: ${user.designation}'),
+              // Add more fields as needed
+            ],
+          ),
         ),
       ),
     );
